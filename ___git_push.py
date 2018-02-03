39c1fd62bf7165a5a63bc4e36a10697f1d9aa3d1
Louis Cuthbert <mun@kerbin.xyz>
2018-02-03 18:53:22 +0000

    Added git push file for ease.

---
+++ ___git_push.py
@@ -0,0 +1,16 @@
+from subprocess import Popen
+import os
+cwd = os.path.dirname(os.path.realpath(__file__)) 
+prefix = ""
+#cmd_string = prefix + "git checkout -b dev"
+#cmd = Popen(cmd_string, shell=True, cwd=cwd)
+cmd_string = prefix + "git add *"
+cmd = Popen(cmd_string, shell=True, cwd=cwd)
+print("Enter Commit Message:")
+commit_message = input()
+cmd_string = prefix + 'git commit -m "{}"'.format(commit_message)
+cmd = Popen(cmd_string, shell=True, cwd=cwd)
+cmd_string = prefix + "git push origin dev"
+cmd = Popen(cmd_string, shell=True, cwd=cwd)
+#
+#	Some login stuff from git

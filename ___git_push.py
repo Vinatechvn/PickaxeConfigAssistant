f3b7753274d215689f0ba359710bd08b4f352d77
Louis Cuthbert <mun@kerbin.xyz>
2018-02-03 20:33:32 +0000

    Updated git_push.py

--- ___git_push.py
+++ ___git_push.py
@@ -3,8 +3,8 @@ import os
 import time
 cwd = os.path.dirname(os.path.realpath(__file__)) 
 prefix = ""
-#cmd_string = prefix + "git checkout -b dev"
-#cmd = Popen(cmd_string, shell=True, cwd=cwd)
+cmd_string = prefix + "git checkout dev"
+cmd = Popen(cmd_string, shell=True, cwd=cwd)
 cmd_string = prefix + "git add *"
 cmd = Popen(cmd_string, shell=True, cwd=cwd)
 print("Enter Commit Message:")
